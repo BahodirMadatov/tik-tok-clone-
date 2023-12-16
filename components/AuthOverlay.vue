90fdb3ffb4e61caaf50a09fc2274619d942b61d0
Bahodir Madatov <n.b.madatov@gmail.com>
2023-12-17 03:59:28 +0500

    tik v0.01

---
+++ components/AuthOverlay.vue
@@ -0,0 +1,13 @@
+<template>
+    <div
+        id="AuthOverlay"
+        class="fixed flex items-center justify-center z-50 top-0 left-0 w-full h-full"
+    >
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
