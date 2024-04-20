3568b02b4841e8c9f25e4634661fc7742564e4b8
Bahodir Madatov <n.b.madatov@gmail.com>
2024-04-20 18:37:05 +0500

    .

--- app.vue
+++ app.vue
@@ -1,4 +1,12 @@
 <template>
   <NuxtPage />
-  <AuthOverlay />
+  <AuthOverlay v-if="isLoginOpen"/>
+  <EditProfileOverlay />
 </template>
+
+
+<script setup>
+import { storeToRefs } from 'pinia';
+const { $generalStore } = useNuxtApp();
+const { isLoginOpen, isEditProfileOpen } = storeToRefs($generalStore);
+</script>
--- components/AuthOverlay.vue
+++ components/AuthOverlay.vue
@@ -5,7 +5,9 @@
     >
         <div class="relative bg-white w-full max-w-[470px] h-[70%] p-4 rounded-lg">
             <div class="w-full flex justify-end">
-                <button class="p-1.5 rounded-full bg-gray-100">
+                <button
+                    @click="$generalStore.isLoginOpen = false"
+                    class="p-1.5 rounded-full bg-gray-100">
                     <Icon name="mdi:close" size="26" color="#000000"/>
                 </button>
             </div>
@@ -28,6 +30,7 @@
 </template>
 
 <script setup>
+const { $generalStore } = useNuxtApp();
 let isRegister = ref(true);
 </script>
 
